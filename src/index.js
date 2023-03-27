0460a3e7e58ecd51394e9a89651a2ea5285ac52f
Utsav Parajuli <utsavparajuli@gmail.com>
2023-03-26 17:18:36 -0700

    changes to index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom";
+import './index.css';
+
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { HashRouter as Router } from "react-router-dom";
 
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,15 +15,16 @@ import { HashRouter as Router } from "react-router-dom";
 //   </React.StrictMode>
 // );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router>
-    <App />
-  </Router>
-);
+ReactDOM.render(<App/>, document.querySelector("#root"))
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Router>
+//     <App />
+//   </Router>
+// );
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
